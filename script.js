7b1d3624f454fb6d4f9b6daac588956526749190
sheks11 <110970118+sheks11@users.noreply.github.com>
2023-06-23 08:02:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ function insert_Row() {
    //Write your code here
    let td1=document.createElement("td");
    let td2=document.createElement("td");
-   td1.innerHTML="New Cell1";
-   td2.innerHTML="New Cell2";
+   td1.innerText="New Cell1";
+   td2.innerText="New Cell2";
    let tr1=document.createElement("tr");
    tr1.appendChild(td1);
    tr1.appendChild(td2);
