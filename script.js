c4bdf8ad54464bc7b2233a4c3a8c9416dfaeaa3e
sheks11 <110970118+sheks11@users.noreply.github.com>
2023-06-23 07:47:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
+let table=document.getElementById("sampleTable");
+
 function insert_Row() {
-    //Write your code here
-  
-  
+   //Write your code here
+   let td1=document.createElement("td");
+   let td2=document.createElement("td");
+   td1.innerHTML="New Cell1";
+   td2.innerHTML="New Cell2";
+   let tr1=document.createElement("tr");
+   tr1.appendChild(td1);
+   tr1.appendChild(td2);
+   table.insertBefore(tr1,table.firstChild);
 }
